90d9b4656df374330819ab7ff0620ea192b8de6b
VilenaMuzipova <94709259+VilenaMuzipova@users.noreply.github.com>
2022-05-20 21:13:54 +0300

    kr2 num2.py

---
+++ kr2 num2.py
@@ -0,0 +1,20 @@
+import numpy
+import time
+
+startTime = time.time()  # время начала замера
+
+items = numpy.random.randint(100000, size=100000)
+print(len(items), items[:10 ** 9])
+a = sum(items)
+print(a)
+if a % 2 == 0:
+    print("сумма четная")
+else:
+    print("нечетная")
+
+endTime = time.time()  # время конца замера
+totalTime = endTime - startTime  # вычисляем затраченное время
+print("Время, затраченное на выполнение данного кода = ", totalTime)
+
+
+
